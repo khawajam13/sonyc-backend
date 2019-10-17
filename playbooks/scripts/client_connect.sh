f24b4aa1a25715a89c88342d3169e083538582df
sonyc <sonyc@master1.nyu.edu>
2019-10-16 21:48:18 -0400

    fixes

--- playbooks/scripts/client_connect.sh
+++ playbooks/scripts/client_connect.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 /bin/echo "Node connected: $common_name with IP: $ifconfig_pool_remote_ip"
-/bin/sed --in-place '/$common_name/d' /etc/openvpn/hosts
+
+/bin/sed --in-place "/$common_name/d" /etc/openvpn/hosts
+
 /bin/echo $ifconfig_pool_remote_ip $common_name >> /etc/openvpn/hosts
--- playbooks/scripts/client_disconnect.sh
+++ playbooks/scripts/client_disconnect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /bin/echo "Node disconnected: $common_name with IP: $ifconfig_pool_remote_ip"
-/bin/sed --in-place '/$common_name/d' /etc/openvpn/hosts
+/bin/sed --in-place "/$common_name/d" /etc/openvpn/hosts
