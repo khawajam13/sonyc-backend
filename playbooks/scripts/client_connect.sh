8f79816362a073a39f891e830d70e2c911df915c
sonyc <sonyc@master1.nyu.edu>
2019-10-17 23:34:00 -0400

    More robust to reboots - NEED TO HANDLE MASTER REBOOT AS IT BRINGS DOWN NFS SO OPENVPN HANGS WHILE MASTER IS DOWN

--- playbooks/scripts/client_connect.sh
+++ playbooks/scripts/client_connect.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 /bin/echo "Node connected: $common_name with IP: $ifconfig_pool_remote_ip"
-
 /bin/sed --in-place "/$common_name/d" /etc/openvpn/hosts
-
 /bin/echo $ifconfig_pool_remote_ip $common_name >> /etc/openvpn/hosts
